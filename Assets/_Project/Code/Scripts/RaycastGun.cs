024dbc341a7f9e513f6f2a67ee7d9195470a0b6c
Nshtk <slava2034@gmail.com>
2024-05-25 03:01:36 +0300

    Made raycast input based

--- Assets/_Project/Code/Scripts/RaycastGun.cs
+++ Assets/_Project/Code/Scripts/RaycastGun.cs
@@ -1,16 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.XR;
-using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.XR.Interaction.Toolkit.Inputs;
 
 [RequireComponent(typeof(LineRenderer))]
 public class RaycastGun : MonoBehaviour
 {
-	public GameObject controller;
+	//public GameObject gun;
+	public ActionBasedController controller;
 	public Transform laserOrigin;
 	public float gunRange = 5f;
 	public float fireRate = 0.002f;
@@ -27,22 +24,17 @@ public class RaycastGun : MonoBehaviour
 	}
 
 	void Update()
-	{	
-
+	{
 		fireTimer += Time.deltaTime;
 		
-		while (fireTimer > fireRate)
+		if (controller.activateAction.action.ReadValue<float>()>0 && fireTimer > fireRate)
 		{
 			fireTimer = 0;
-
-			
-			
 			laserLine.SetPosition(0, laserOrigin.position);
-			Vector3 rayOrigin = controller.transform.transform.position;
 			Vector3 spread = new Vector3(Random.value * 0.5f - 0.25f, Random.value * 0.5f - 0.25f, Random.value * 0.5f - 0.25f);
 			Vector3 direction = controller.transform.forward + spread;
 			RaycastHit hit;
-			if (Physics.Raycast(rayOrigin, direction, out hit, gunRange))
+			if (Physics.Raycast(controller.transform.transform.position, direction, out hit, gunRange))
 			{
 				laserLine.SetPosition(1, hit.point);
 				Instantiate(LazerPoint, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
